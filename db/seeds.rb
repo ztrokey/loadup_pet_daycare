be00cafc83abd50e5f982b92740ddd91e07fbd05
Zach T <ztrokey@gmail.com>
2023-05-10 09:18:42 -0600

    style: Add styling to pages and seed db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+1.upto(10) do |i|
+  animals = %w[Dog Cat]
+  hours = (2..8).to_a
+  date = Time.now
+  Booking.create(first_name: "Alpha#{i}",
+                 last_name: "Beta#{i}",
+                 animal_name: "Charlie#{i}",
+                 animal_type: animals.sample,
+                 hours_requested: hours.sample,
+                 date_of_service: date + i.days)
+end
